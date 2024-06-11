d65b3f54b587640d434b7fb7bab2a7dccfba5d6b
Arxivar Abletech <Arxivar@users.noreply.github.com>
2024-06-11 16:00:19 +0200

    Aggiunta regola eslint no-missing-key-prop

---
+++ rules/no-missing-key-prop.js
@@ -0,0 +1,46 @@
+/** @type {import('eslint').Rule.RuleModule} */
+module.exports = {
+    meta: {
+        type: 'problem',
+        docs: {
+            description: 'disallow missing key prop in elements inside a list',
+            category: 'Possible Errors',
+            recommended: true,
+        },
+        fixable: 'code',
+        schema: [] // no options
+    },
+    create: function(context) {
+        return {
+            CallExpression(node) {
+                if (
+                    node.callee.type === 'MemberExpression' &&
+                    node.callee.object.name === '_' &&
+                    node.callee.property.name === 'map'
+                ) {
+                    const callback = node.arguments[1];
+                    if (callback && callback.type === 'ArrowFunctionExpression') {
+                        callback.body.body.forEach(statement => {
+                            if (
+                                statement.type === 'ReturnStatement' &&
+                                statement.argument.type === 'JSXElement'
+                            ) {
+                                const jsxElement = statement.argument;
+                                const hasKey = jsxElement.openingElement.attributes.some(
+                                    attr => attr.type === 'JSXAttribute' && attr.name.name === 'key'
+                                );
+
+                                if (!hasKey) {
+                                    context.report({
+                                        node: jsxElement,
+                                        message: 'Missing "key" prop in JSX element within a list',
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        };
+    }
+};
