057b4dee37bb088daacf2a4c68f6fa83f9950f21
Arxivar Abletech <Arxivar@users.noreply.github.com>
2024-06-12 11:49:31 +0200

    Update recommended.js

--- configs/recommended.js
+++ configs/recommended.js
@@ -1,6 +1,6 @@
 module.exports = {
   rules: {
-    'arxivar/filename-starts-with': 2,
+    'arxivar/no-relative-import': 2,
     'arxivar/no-use-internals': 2,
     'arxivar/toaster-service-unsafe-methods': 2,
     'arxivar/no-missing-key-prop': 2
