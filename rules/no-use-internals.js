99f7422edda7196844de21495b800159022c12e6
Arxivar Abletech <Arxivar@users.noreply.github.com>
2024-06-12 11:18:37 +0200

    Update no-use-internals.js

--- rules/no-use-internals.js
+++ rules/no-use-internals.js
@@ -1,11 +1,9 @@
-const path = require('path');
-
 /** @type {import('eslint').Rule.RuleModule} */
 module.exports = {
     meta: {
         type: 'problem',
         docs: {
-            description: 'disallow imports from "Internals" folder if the importing file is not in the same root directory',
+            description: 'disallow imports from "Internals" folder unless preceded by "./"',
             category: 'Best Practices',
             recommended: false,
         },
@@ -15,26 +13,11 @@ module.exports = {
         return {
             ImportDeclaration(node) {
                 const importPath = node.source.value;
-                const filename = context.getFilename();
-
-                if (importPath.includes('Internals')) {
-                    const importFullPath = path.resolve(path.dirname(filename), importPath);
-                    const importDir = path.dirname(importFullPath);
-
-                    const getRootDir = (filePath) => {
-                        const parts = filePath.split(path.sep);
-                        return parts.length > 1 ? parts[0] : null;
-                    };
-
-                    const importingFileRootDir = getRootDir(path.relative(context.getCwd(), filename));
-                    const internalsFolderRootDir = getRootDir(path.relative(context.getCwd(), importDir));
-
-                    if (importingFileRootDir !== internalsFolderRootDir) {
-                        context.report({
-                            node,
-                            message: 'Imports from "Internals" folder are not allowed if the importing file is not in the same root directory.',
-                        });
-                    }
+                if (importPath.includes('/Internals/') && !importPath.startsWith('./Internals/')) {
+                    context.report({
+                        node,
+                        message: 'Imports from "Internals" folder must be preceded by "./".',
+                    });
                 }
             }
         };
