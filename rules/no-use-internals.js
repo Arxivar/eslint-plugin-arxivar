59c52a1d23a6bf4365d1b3b5e46e5ea12a8736d0
Arxivar Abletech <Arxivar@users.noreply.github.com>
2024-06-12 11:06:48 +0200

    Update no-use-internals.js

--- rules/no-use-internals.js
+++ rules/no-use-internals.js
@@ -5,7 +5,7 @@ module.exports = {
     meta: {
         type: 'problem',
         docs: {
-            description: 'disallow imports from "Internals" folder if the importing file is not in the same parent directory',
+            description: 'disallow imports from "Internals" folder if the importing file is not in the same root directory',
             category: 'Best Practices',
             recommended: false,
         },
@@ -21,15 +21,18 @@ module.exports = {
                     const importFullPath = path.resolve(path.dirname(filename), importPath);
                     const importDir = path.dirname(importFullPath);
 
-                    const importingFileDir = path.dirname(filename);
-                    const importingFileParentDir = path.dirname(importingFileDir);
+                    const getRootDir = (filePath) => {
+                        const parts = filePath.split(path.sep);
+                        return parts.length > 1 ? parts[0] : null;
+                    };
 
-                    const internalsFolderParentDir = path.dirname(importDir);
+                    const importingFileRootDir = getRootDir(path.relative(context.getCwd(), filename));
+                    const internalsFolderRootDir = getRootDir(path.relative(context.getCwd(), importDir));
 
-                    if (importingFileParentDir !== internalsFolderParentDir) {
+                    if (importingFileRootDir !== internalsFolderRootDir) {
                         context.report({
                             node,
-                            message: 'Imports from "Internals" folder are not allowed if the importing file is not in the same parent directory.',
+                            message: 'Imports from "Internals" folder are not allowed if the importing file is not in the same root directory.',
                         });
                     }
                 }
